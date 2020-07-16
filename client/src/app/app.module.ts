afc932e2b98e56fbbc16820e02a6b2070cd4021c
MendozaMiguel <samirmiguelvoillaume@gmail.com>
2020-07-16 20:33:06 +0200

    declare api service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ApiClientService } from './api-client/api-client.service';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ApiClientService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
