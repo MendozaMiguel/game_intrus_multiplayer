bb060644582a44498144309dfcd22b6a625e38d3
MendozaMiguel <samirmiguelvoillaume@gmail.com>
2020-07-23 20:31:26 +0200

    add socket new user

--- server/config/functions/bootstrap.js
+++ server/config/functions/bootstrap.js
@@ -10,16 +10,28 @@
 
 module.exports = cb => {
   // import socket io
-  var io = require('socket.io')(strapi.server);
+
+  const users = {}
+
+  const io = require('socket.io')(strapi.server);
   // listen for user connection
   io.on('connection', (socket) => {
+    socket.on('new-user', name => {
+      users[socket.id] = name;
+      socket.broadcast.emit('user-connected', name)
+      console.log('name : ', name);
+    })
     // send message on user connection
-    console.log('user connected');
     socket.on('new-message', (message) => {
-      io.sockets.emit('new-message', message)
+      socket.broadcast.emit('new-message', {
+        message: message,
+        name: users[socket.id]
+      })
     });
     // listen for user diconnect
-    socket.on('disconnect', () => console.log('a user disconnected'));
+    socket.on('disconnect', () => {
+      socket.broadcast.emit('user-disconnected', users[socket.id])
+    });
   });
   strapi.io = io; // register socket io inside strapi main object to use it globally anywhere
 
